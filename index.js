52e0abc2768a86717749fc09c905ea637d7fc632
liconeto <benicio.neto9@gmail.com>
2023-02-14 17:57:40 -0300

    Exercise MyCity

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const city = require("./city.json");
-const states = require("./states.json");
+const insertedCity = document.getElementById("city");
 
-let minhaCidade = "Alegre";
+async function myCity() {
+  const searchCity = insertedCity.value;
+  const dataCities = await fetch("./city.json").then((res) => res.json());
+  const dataEstates = await fetch("./states.json").then((res) => res.json());
 
-const myCity = () => {
-  for (const x in city) {
-    const element = city[x];
-    if (element.Nome === minhaCidade) {
-      return element;
+  let paragrafo = document.createElement("p");
+  let objCity;
+  let objState;
+
+  for (x in dataCities) {
+    objCity = dataCities[x];
+
+    if (searchCity === objCity.Nome) {
+      paragrafo.innerText += `${searchCity} | Id estado = ${objCity.Estado} | Id cidade = ${objCity.ID} | ${objCity.Nome}`;
+      for (State in dataEstates) {
+        objState = dataEstates[State];
+
+        if (objState.ID === objCity.Estado) {
+          paragrafo.innerText += `${objState.Nome} \n\n\tAguarde buscando as cidade do seu estado! \n`;
+        }
+      }
     }
-  }
-};
 
-const stateId = () => {
-  return myCity().Estado;
-};
-
-const stateName = (id) => {
-  for (const e in states) {
-    const element = states[e];
-    if (element.ID === id) {
-      return element.Nome;
-    }
+    console.log(objCity);
+    document.body.appendChild(paragrafo);
   }
-};
+}
 
 const listOfCitiesOfMyEstate = () => {
   const idState = stateId();
@@ -40,8 +43,4 @@ const listOfCitiesOfMyEstate = () => {
   }
   return cities;
 };
-
-console.log(myCity());
-console.log(stateId(myCity()));
-console.log("|" + Object.values(myCity()) + "|" + stateName(stateId()) + " |");
-console.log(listOfCitiesOfMyEstate(stateId(myCity())));
+s;
