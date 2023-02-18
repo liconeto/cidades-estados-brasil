e55588873b4be6ffdda27afb53cebc159a3185cb
Liconeto9 <benicio.neto9@gmail.com>
2023-02-17 22:10:03 -0300

    Finish exercise City of my State

--- index.js
+++ index.js
@@ -5,14 +5,38 @@ async function myCity() {
 
   let insertedCity = document.querySelector("input#city");
   let searchCity = String(insertedCity.value);
-
+  let stateCity = "";
+  let getStateName = "";
   let paragrafo = document.createElement("p");
   paragrafo.innerText += `${searchCity} \n`;
 
+  let textBox = document.createElement("textarea");
+
   for (cities in dataCities) {
     let value = dataCities[cities];
-    if(value.Estado === )
-    paragrafo.innerText += `Estado: ${value.Estado} cidade: ${value.Nome}`;
+
+    if (searchCity === value.Nome) {
+      stateCity = value.Estado;
+    }
   }
+
+  for (stateOfCity in dataEstates) {
+    let data = dataEstates[stateOfCity];
+    if (stateCity === data.ID) {
+      getStateName = data.Nome;
+    }
+  }
+
+  paragrafo.innerText += `Sua cidade ${searchCity} fica no estado ${getStateName} `;
+
+  paragrafo.innerText += `Aguarde um instante vamos busacar as cidades do seu estado!`;
   document.getElementById("response").appendChild(paragrafo);
+  for (estado in dataCities) {
+    let dataState = dataCities[estado];
+    if (dataState.Estado === stateCity) {
+      textBox.innerText += `${dataState.Nome}; `;
+    }
+  }
+
+  document.getElementById("response").appendChild(textBox);
 }
