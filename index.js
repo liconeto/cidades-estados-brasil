a97fb556451c8ac6ed0669da82e677fadc799801
liconeto <benicio.neto9@gmail.com>
2023-02-17 22:04:10 -0300

    update

--- index.js
+++ index.js
@@ -1,46 +1,18 @@
-const insertedCity = document.getElementById("city");
-
+//let dados &{};
 async function myCity() {
-  const searchCity = insertedCity.value;
   const dataCities = await fetch("./city.json").then((res) => res.json());
   const dataEstates = await fetch("./states.json").then((res) => res.json());
 
+  let insertedCity = document.querySelector("input#city");
+  let searchCity = String(insertedCity.value);
+
   let paragrafo = document.createElement("p");
-  let objCity;
-  let objState;
+  paragrafo.innerText += `${searchCity} \n`;
 
-  for (x in dataCities) {
-    objCity = dataCities[x];
-
-    if (searchCity === objCity.Nome) {
-      paragrafo.innerText += `${searchCity} | Id estado = ${objCity.Estado} | Id cidade = ${objCity.ID} | ${objCity.Nome}`;
-      for (State in dataEstates) {
-        objState = dataEstates[State];
-
-        if (objState.ID === objCity.Estado) {
-          paragrafo.innerText += `${objState.Nome} \n\n\tAguarde buscando as cidade do seu estado! \n`;
-        }
-      }
-    }
-
-    console.log(objCity);
-    document.body.appendChild(paragrafo);
+  for (cities in dataCities) {
+    let value = dataCities[cities];
+    if(value.Estado === )
+    paragrafo.innerText += `Estado: ${value.Estado} cidade: ${value.Nome}`;
   }
+  document.getElementById("response").appendChild(paragrafo);
 }
-
-const listOfCitiesOfMyEstate = () => {
-  const idState = stateId();
-  let cities = [];
-  for (const x in city) {
-    const element = city[x];
-    if (element.Estado === idState) {
-      cities.push({
-        id: `${element.ID}`,
-        nome: `${element.Nome}`,
-        estado: `${element.Estado}`,
-      });
-    }
-  }
-  return cities;
-};
-s;
